62b0dc0afab3fddb9f6604e64fd85fcecf63faa8
Kai <k@limist.com>
2011-02-08 16:12:29 -0500

    Minor updates, changed author to myself for expediency of future contact.

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'python-money',
-    version = '0.2',
-    url = 'http://github.com/limist/python-money',
-    license = 'BSD',
-    description = 'python-money provides carefully designed basic Python primitives for working with money and currencies.',
-    author = 's3x3y1, ppr.vitaly, efilipov@mlke.net, Kai Wu',
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    install_requires = ['setuptools'],
+    name='python-money',
+    version='0.2',
+    url='http://github.com/limist/python-money',
+    license='BSD',
+    author='Kai',
+    author_email='k@limist.com',
+    description='python-money provides re-usable Currency and Money classes for working with money and currencies in your own software.',
+    keywords="money currency class abstraction",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['setuptools'],
     )
