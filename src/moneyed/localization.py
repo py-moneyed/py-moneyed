7756cd63862c2e426b840a94accd5944260b2405
Johannes Hoppe <info@johanneshoppe.com>
2013-12-11 21:22:33 +0100

    Adds format for german speaking countries

--- src/moneyed/localization.py
+++ src/moneyed/localization.py
@@ -139,6 +139,21 @@ _format("en_US", group_size=3, group_separator=",", decimal_point=".",
                  positive_sign="", trailing_positive_sign="",
                  negative_sign="-", trailing_negative_sign="",
                  rounding_method=ROUND_HALF_EVEN)
+                 
+_format("de_DE", group_size=3, group_separator=" ", decimal_point=",",
+                 positive_sign="", trailing_positive_sign="",
+                 negative_sign="-", trailing_negative_sign="",
+                 rounding_method=ROUND_HALF_EVEN)
+                 
+_format("de_AT", group_size=3, group_separator=" ", decimal_point=",",
+                 positive_sign="", trailing_positive_sign="",
+                 negative_sign="-", trailing_negative_sign="",
+                 rounding_method=ROUND_HALF_EVEN)
+                 
+_format("de_CH", group_size=3, group_separator=" ", decimal_point=".",
+                 positive_sign="", trailing_positive_sign="",
+                 negative_sign="-", trailing_negative_sign="",
+                 rounding_method=ROUND_HALF_EVEN)
 
 _format("sv_SE", group_size=3, group_separator=" ", decimal_point=",",
                  positive_sign="", trailing_positive_sign="",
