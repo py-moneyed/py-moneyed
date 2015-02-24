7c5cbd8a58bd1d166bf1655ae13140e0b0f4de72
Hasan Turksoy <hasan@udemy.com>
2015-02-25 01:10:52 +0200

    make Money class open for extention

--- src/moneyed/classes.py
+++ src/moneyed/classes.py
@@ -78,12 +78,12 @@ class Money(object):
         return format_money(self)
 
     def __pos__(self):
-        return Money(
+        return self.__class__(
             amount=self.amount,
             currency=self.currency)
 
     def __neg__(self):
-        return Money(
+        return self.__class__(
             amount= -self.amount,
             currency=self.currency)
 
@@ -92,7 +92,7 @@ class Money(object):
             raise TypeError('Cannot add or subtract a ' +
                             'Money and non-Money instance.')
         if self.currency == other.currency:
-            return Money(
+            return self.__class__(
                 amount=self.amount + other.amount,
                 currency=self.currency)
 
@@ -106,7 +106,7 @@ class Money(object):
         if isinstance(other, Money):
             raise TypeError('Cannot multiply two Money instances.')
         else:
-            return Money(
+            return self.__class__(
                 amount=(self.amount * Decimal(str(other))),
                 currency=self.currency)
 
@@ -116,12 +116,12 @@ class Money(object):
                 raise TypeError('Cannot divide two different currencies.')
             return self.amount / other.amount
         else:
-            return Money(
+            return self.__class__(
                 amount=self.amount / Decimal(str(other)),
                 currency=self.currency)
 
     def __abs__(self):
-        return Money(
+        return self.__class__(
             amount=abs(self.amount),
             currency=self.currency)
 
@@ -138,7 +138,7 @@ class Money(object):
         if isinstance(other, Money):
             raise TypeError('Invalid __rmod__ operation')
         else:
-            return Money(
+            return self.__class__(
                 amount=(Decimal(str(other)) * self.amount / 100),
                 currency=self.currency)
 
