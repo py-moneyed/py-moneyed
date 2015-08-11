fcec79aa61c3c9fd20be8372e449db4993b64808
Luke Plant <L.Plant.98@cantab.net>
2015-08-11 11:49:39 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class Tox(TestCommand):
 setup(
     name='py-moneyed',
     packages=['moneyed'],
-    version='0.5.0',
+    version='0.6.0',
     description='Provides Currency and Money classes for use in your Python code.',
     author='Kai',
     author_email='k@limist.com',
