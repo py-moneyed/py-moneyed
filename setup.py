f8584166a799c08c95c0c8247915257655fcad0c
Joseph Metzinger <jj@mozio.com>
2016-04-29 08:59:37 -0700

    bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class Tox(TestCommand):
 setup(
     name='py-moneyed',
     packages=['moneyed'],
-    version='0.6.1',
+    version='0.6.2',
     description='Provides Currency and Money classes for use in your Python code.',
     author='Kai',
     author_email='k@limist.com',
