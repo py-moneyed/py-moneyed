02ec5fd0f3f5d6a41f43ffdcf1d597d6df04f819
Erwin Junge <erwin@junge.nl>
2016-08-18 19:22:29 +0200

    PEP-8 is sacred

--- moneyed/classes.py
+++ moneyed/classes.py
@@ -447,7 +447,12 @@ XPT = add_currency('XPT', '962', 'Platinum', [])
 XSU = add_currency('XSU', '994', 'Sucre', ['SISTEMA UNITARIO DE COMPENSACION REGIONAL DE PAGOS "SUCRE"'])
 XTS = add_currency('XTS', '963', 'Codes specifically reserved for testing purposes', [])
 XUA = add_currency('XUA', '965', 'ADB Unit of Account', ['MEMBER COUNTRIES OF THE AFRICAN DEVELOPMENT BANK GROUP'])
-XXX = add_currency('XXX', '999', 'The codes assigned for transactions where no currency is involved', ['ZZ07_No_Currency'])
+XXX = add_currency(
+    'XXX',
+    '999',
+    'The codes assigned for transactions where no currency is involved',
+    ['ZZ07_No_Currency'],
+)
 YER = add_currency('YER', '886', 'Yemeni Rial', ['YEMEN'])
 ZAR = add_currency('ZAR', '710', 'Rand', ['SOUTH AFRICA'])
 ZMK = add_currency('ZMK', '894', 'Zambian Kwacha', [])  # historical
