abfdb75463e020bd6e7e2e052070a445eaed7274
Erwin Junge <erwin@junge.nl>
2016-08-18 18:57:30 +0200

    Add support for XXX

--- moneyed/classes.py
+++ moneyed/classes.py
@@ -447,6 +447,7 @@ XPT = add_currency('XPT', '962', 'Platinum', [])
 XSU = add_currency('XSU', '994', 'Sucre', ['SISTEMA UNITARIO DE COMPENSACION REGIONAL DE PAGOS "SUCRE"'])
 XTS = add_currency('XTS', '963', 'Codes specifically reserved for testing purposes', [])
 XSU = add_currency('XSU', '965', 'ADB Unit of Account', ['MEMBER COUNTRIES OF THE AFRICAN DEVELOPMENT BANK GROUP'])
+XXX = add_currency('XXX', '999', 'The codes assigned for transactions where no currency is involved', ['ZZ07_No_Currency'])
 YER = add_currency('YER', '886', 'Yemeni Rial', ['YEMEN'])
 ZAR = add_currency('ZAR', '710', 'Rand', ['SOUTH AFRICA'])
 ZMK = add_currency('ZMK', '894', 'Zambian Kwacha', [])  # historical
