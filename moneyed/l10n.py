c0b219baa539d5f225d47647d18e816fe55f0eca
Pouya Mobasher Behrouz <pooyamb@gmail.com>
2019-05-30 10:45:52 +0430

    add comment about unused rounding_method

--- moneyed/l10n.py
+++ moneyed/l10n.py
@@ -11,11 +11,12 @@ def format_currency(money, *args, **kwargs):
     return babel_format_currency(money.amount, money.currency.code, *args, **kwargs)
 
 
-DECIMAL_PLACES_REGEX = re.compile('\.0*')
+DECIMAL_PLACES_REGEX = re.compile(r'\.0*')
 
 
 def format_money(money, include_symbol=True, locale=LC_NUMERIC,
                  decimal_places=None, rounding_method=None):
+    # FIXME: rounding_method is doing nothing
     locale = Locale.parse(locale)
     format = locale.currency_formats['standard'].pattern
     if not include_symbol:
