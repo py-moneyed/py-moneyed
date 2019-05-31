5ea9942981accaeee1787c30c4b7f2899d775a36
Pouya Mobasher Behrouz <pooyamb@gmail.com>
2019-05-31 05:44:36 +0430

    Settings default locale to 'en_US' for backward compatibility and tests

--- moneyed/classes.py
+++ moneyed/classes.py
@@ -19,11 +19,11 @@ from .loader import load_currencies
 DEFAULT_CURRENCY = add_currency(DEFAULT_CURRENCY_CODE, '999', 'Default currency.', [])
 
 
-load_currencies()
+load_currencies(locale='en_US')
 
 # obslete currencies
 load_currencies(['BYR', 'LTL', 'LVL', 'MRO', 'STD', 'TMM', 'VEF',
-                 'XFO', 'XFU', 'ZMK', 'ZWD', 'ZWL'])
+                 'XFO', 'XFU', 'ZMK', 'ZWD', 'ZWL'], locale='en_US')
 
 # currencies not supported by babel
 IMP = add_currency('IMP', 'Nil', 'Isle of Man Pound', ['ISLE OF MAN'])
