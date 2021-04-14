c14378fa7f80f7adb6339b3b4f3ccb512d10f046
Anton Agestam <git@antonagestam.se>
2021-04-14 20:18:57 +0200

    Introduce black and use pre-commit in CI

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'py-moneyed'
-copyright = '2021, Kai Wu'
-author = 'Kai Wu'
+project = "py-moneyed"
+copyright = "2021, Kai Wu"
+author = "Kai Wu"
 
 # The full version, including alpha/beta/rc tags
-release = '1.2'
+release = "1.2"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,16 +30,15 @@ release = '1.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
--- setup.py
+++ setup.py
@@ -15,49 +15,47 @@ class Tox(TestCommand):
     def run_tests(self):
         # import here, cause outside the eggs aren't loaded
         import tox
+
         errno = tox.cmdline(self.test_args)
         sys.exit(errno)
 
 
 setup(
-    name='py-moneyed',
-    packages=['moneyed'],
-    version='1.2',
-    description='Provides Currency and Money classes for use in your Python code.',
-    author='Kai',
-    author_email='k@limist.com',
+    name="py-moneyed",
+    packages=["moneyed"],
+    version="1.2",
+    description="Provides Currency and Money classes for use in your Python code.",
+    author="Kai",
+    author_email="k@limist.com",
     maintainer="Dmitry Dygalo",
     maintainer_email="dadygalo@gmail.com",
-    url='http://github.com/py-moneyed/py-moneyed',
-    download_url='',
+    url="http://github.com/py-moneyed/py-moneyed",
+    download_url="",
     keywords="money currency class abstraction",
-    license='BSD',
+    license="BSD",
     install_requires=[
-        'babel>=2.8.0',
+        "babel>=2.8.0",
     ],
     classifiers=[
-        'Programming Language :: Python',
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Development Status :: 6 - Mature',
-        'Environment :: Other Environment',
-        'Intended Audience :: Developers',
-        'Topic :: Office/Business :: Financial',
-        'Topic :: Software Development :: Libraries :: Python Modules'],
-    long_description=open('README.rst').read(),
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Development Status :: 6 - Mature",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Topic :: Office/Business :: Financial",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    long_description=open("README.rst").read(),
     long_description_content_type="text/x-rst",
-    extras_require={
-        'tests': [
-            'pytest>=2.3.0',
-            'tox>=1.6.0'
-        ]},
-    tests_require=['tox>=1.6.0', 'pytest>=2.3.0'],
-    cmdclass={'test': Tox},
+    extras_require={"tests": ["pytest>=2.3.0", "tox>=1.6.0"]},
+    tests_require=["tox>=1.6.0", "pytest>=2.3.0"],
+    cmdclass={"test": Tox},
     include_package_data=True,
 )
